e263a45bd99e55c669ff7dfb343c431b99a51683
Yusuf Ismail <yusuf@qfsw.co.uk>
2019-10-22 15:52:47 +0100

    Added bias support for the motors

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ port_left = brickpi3.BrickPi3.PORT_D
 port_right = brickpi3.BrickPi3.PORT_A
 wheel_radius = 3.5 / 100 # 3.5cm
 body_radius = 7.2 / 100 # 8cm
-robot = movement.MovementModule(port_left, port_right, wheel_radius, body_radius)
+bisd = 0.2 # 20%
+robot = movement.MovementModule(port_left, port_right, wheel_radius, body_radius, bias)
 
 try:
     for i in range(4):
