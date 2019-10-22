cc328dbd87ceb6e43f146a7099adac4e7e38d47f
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-10-22 13:08:37 +0100

    variable speed control

--- src/remote_controller.py
+++ src/remote_controller.py
@@ -2,6 +2,8 @@ import keyboard
 import movement, brickpi3
 import curses
 
+
+
 #this script can only be run as root
 port_left = brickpi3.BrickPi3.PORT_D
 port_right = brickpi3.BrickPi3.PORT_A
@@ -9,10 +11,15 @@ wheel_radius = 3.5 / 100 # 3.5cm
 body_radius = 9.1 / 100 # 9.1cm
 robot = movement.MovementModule(port_left, port_right, wheel_radius, body_radius)
 
-
+ 
 def main(stdscr):
     # do not wait for input when calling getch
     stdscr.nodelay(1)
+    lin_speed = 10
+    turn_speed = 90
+
+    is_moving_fwd = False
+    is_turning = False
 
     while True:
         # get keyboard input, returns -1 if none available
@@ -20,20 +27,40 @@ def main(stdscr):
         if c != -1:
             print('value of c ')
             print(c)
+
+            if c >='1' and c <= '5':
+                lin_speed = c - '1'
+                lin_speed = 0.1 + lin_speed
+                if is_moving_fwd:
+                    robot.set_linear_speed(lin_speed)
+
+            if c >='6' and c <= '9':
+                turn_speed = c - '6'
+                turn_speed = 5 + turn_speed * 25
+                if is_turning:
+                    robot.set_turn_speed(turn_speed)
+
             
             if c == 259: #up arrow
-                 robot.set_linear_speed(-10)
+                 robot.set_linear_speed(lin_speed)
+                 is_moving_fwd = True
             elif c == 258: #down arrow
-                robot.set_linear_speed(10)
+                robot.set_linear_speed(-1 * lin_speed)
+                is_moving_fwd = True
+
             elif c == 32: #space bar
                 robot.set_linear_speed(0)
-                
+                is_moving_fwd = False
+
             if c == 261: #right arrow
-                robot.set_turn_speed(-90)
+                robot.set_turn_speed(turn_speed)
+                is_turning = True
             elif c == 260: #left arrow
-                robot.set_turn_speed(90)
+                robot.set_turn_speed(-1 * turn_speed)
+                is_turning = True
             elif c == 32: #space bar
                 robot.set_turn_speed(0)
+                is_turning = False
                 
                 
             
