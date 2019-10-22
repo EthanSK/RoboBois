88c7226127b266d29eaad378270d1015c72ad1c3
Yusuf Ismail <yusuf@qfsw.co.uk>
2019-10-22 16:01:19 +0100

    Disabled bias

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ port_left = brickpi3.BrickPi3.PORT_D
 port_right = brickpi3.BrickPi3.PORT_A
 wheel_radius = 3.5 / 100 # 3.5cm
 body_radius = 7.2 / 100 # 8cm
-bisd = 0.2 # 20%
-robot = movement.MovementModule(port_left, port_right, wheel_radius, body_radius, bias)
+robot = movement.MovementModule(port_left, port_right, wheel_radius, body_radius)
 
 try:
     for i in range(4):
