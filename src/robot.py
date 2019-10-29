1b0829bbfaa90b22015a079eadf369278ecf11b8
ethansk <ethaneus99@gmail.com>
2019-10-29 13:57:49 +0000

    Added Robot wrapper class

---
+++ src/robot.py
@@ -0,0 +1,6 @@
+import brickpi3, movement, sensor
+
+class Robot:
+    def __init__(self, movement_module, sensor_module):
+        self.movement_module = movement_module
+        self.sensor_module = sensor_module
