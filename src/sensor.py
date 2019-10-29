98784d5a38b9194907b81abff65d94f3a97bf8e3
ethansk <ethaneus99@gmail.com>
2019-10-29 13:55:12 +0000

    Begun work on the sensor module

--- src/sensor.py
+++ src/sensor.py
@@ -1,12 +1,19 @@
 import brickpi3, math
 
 class SensorModule:
-    
-    def __init__(self, lmotor, rmotor, wh_radius, bd_radius, bias = 0):
+    def __init__(self, ltouch, rtouch, sonar):
         self.BP = brickpi3.BrickPi3()
-        BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH)
+        self.ltouch = ltouch
+        self.rtouch = rtouch
+        self.sonar = sonar
 
+        BP.set_sensor_type(ltouch, BP.SENSOR_TYPE.TOUCH)
+        BP.set_sensor_type(rtouch, BP.SENSOR_TYPE.TOUCH)
+        BP.set_sensor_type(sonar, BP.SENSOR_TYPE.SONAR)
+    
+    def get_left_touch(self):
+        return self.BP.get_sensor(ltouch)
+    
+    def get_right_touch(self):
+        return self.BP.get_sensor(rtouch)
 
-    def set_left_dps(self, dps):
-        self.BP.set_motor_limits(self.lmotor, MovementModule.max_power, MovementModule.max_dps)
-        self.BP.set_motor_dps(self.lmotor, dps)
