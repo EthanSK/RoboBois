6b5e0f25bd3bd7ce5ba7278328192dac56d5bd6f
ethansk <ethaneus99@gmail.com>
2019-11-05 12:39:58 +0000

    Added smooth sonar and added sonar hotkey to remote controller

--- src/remote_controller.py
+++ src/remote_controller.py
@@ -1,5 +1,7 @@
 import keyboard
 import movement
+import sensor
+import robot
 import brickpi3
 import curses
 import sounds
@@ -7,12 +9,19 @@ from enum import Enum
 
 
 # this script can only be run as root
-port_left = brickpi3.BrickPi3.PORT_D
-port_right = brickpi3.BrickPi3.PORT_A
+motor_port_left = brickpi3.BrickPi3.PORT_D
+motor_port_right = brickpi3.BrickPi3.PORT_A
+touch_port_left = brickpi3.BrickPi3.PORT_4
+touch_port_right = brickpi3.BrickPi3.PORT_1
+sonar_port = brickpi3.BrickPi3.PORT_2
+
 wheel_radius = 3.5 / 100  # 3.5cm
 body_radius = 9.1 / 100  # 9.1cm
-robot = movement.MovementModule(
-    port_left, port_right, wheel_radius, body_radius)
+movement = movement.MovementModule(
+    motor_port_left, motor_port_right, wheel_radius, body_radius)
+sensor_module = sensor.SensorModule(touch_port_left, touch_port_right, sonar_port)
+roboboi = robot.Robot(movement, sensor_module)
+
 
 
 class MovementState(Enum):
@@ -31,10 +40,17 @@ def main(stdscr):
     steer_amount = 0
     steer_step = 0.1
     steer_init_step = 0.3
-    movement = MovementState.NONE
+    movement_state = MovementState.NONE
+    # need_crash_recovery = False
 
     while True:
         # get keyboard input, returns -1 if none available
+
+        if sensor_module.get_left_touch(): 
+            crash_recovery_routine(lin_speed, turn_speed, -1)
+        if sensor_module.get_right_touch(): 
+            crash_recovery_routine(lin_speed, turn_speed, 1)
+
         c = stdscr.getch()
         if c != -1:
             print('value of c ')
@@ -43,51 +59,54 @@ def main(stdscr):
             if c >= 49 and c <= 53:
                 lin_speed = c - 49
                 lin_speed = 0.05 + lin_speed / 15
-                if movement == MovementState.FORWARD:
-                    robot.steer(-lin_speed, steer_amount)
-                elif movement == MovementState.BACKWARD:
-                    robot.steer(lin_speed, steer_amount)
+                if movement_state == MovementState.FORWARD:
+                    movement.steer(-lin_speed, steer_amount)
+                elif movement_state == MovementState.BACKWARD:
+                    movement.steer(lin_speed, steer_amount)
 
             if c >= 54 and c <= 57:
                 turn_speed = c - 54
                 turn_speed = 8 + turn_speed * 30
-                if movement == MovementState.CLOCKWISE:
-                    robot.set_turn_speed(-turn_speed)
-                elif movement == MovementState.ANTICLOCKWISE:
-                    robot.set_turn_speed(turn_speed)
+                if movement_state == MovementState.CLOCKWISE:
+                    movement.set_turn_speed(-turn_speed)
+                elif movement_state == MovementState.ANTICLOCKWISE:
+                    movement.set_turn_speed(turn_speed)
 
-            lin_speed_mul = -1 if movement is MovementState.FORWARD else 1
+            lin_speed_mul = -1 if movement_state is MovementState.FORWARD else 1
 
             if c == 259:  # up arrow
                 steer_amount = 0
-                robot.steer(-lin_speed, steer_amount)
-                movement = MovementState.FORWARD
+                movement.steer(-lin_speed, steer_amount)
+                movement_state = MovementState.FORWARD
             elif c == 258:  # down arrow
                 steer_amount = 0
-                robot.steer(lin_speed, steer_amount)
-                movement = MovementState.BACKWARD
+                movement.steer(lin_speed, steer_amount)
+                movement_state = MovementState.BACKWARD
 
             elif c == 32:  # space bar
                 steer_amount = 0
-                robot.set_linear_speed(0)
-                movement = MovementState.NONE
+                movement.set_linear_speed(0)
+                movement_state = MovementState.NONE
 
             elif c == 261:  # right arrow
-                if movement is MovementState.FORWARD or movement is MovementState.BACKWARD:
+                if movement_state is MovementState.FORWARD or movement_state is MovementState.BACKWARD:
                     if steer_amount is 0: steer_amount = steer_init_step
                     else: steer_amount = min(1, steer_amount + steer_step)
-                    robot.steer(lin_speed * lin_speed_mul, steer_amount)
+                    movement.steer(lin_speed * lin_speed_mul, steer_amount)
                 else:
-                    robot.set_turn_speed(-turn_speed)
-                    movement = MovementState.CLOCKWISE
+                    movement.set_turn_speed(-turn_speed)
+                    movement_state = MovementState.CLOCKWISE
             elif c == 260:  # left arrow
-                if movement is MovementState.FORWARD or movement is MovementState.BACKWARD:
+                if movement_state is MovementState.FORWARD or movement_state is MovementState.BACKWARD:
                     if steer_amount is 0: steer_amount = -steer_init_step
                     else: steer_amount = max(-1, steer_amount - steer_step)
-                    robot.steer(lin_speed * lin_speed_mul, steer_amount)
+                    movement.steer(lin_speed * lin_speed_mul, steer_amount)
                 else:
-                    robot.set_turn_speed(turn_speed)
-                    movement = MovementState.ANTICLOCKWISE
+                    movement.set_turn_speed(turn_speed)
+                    movement_state = MovementState.ANTICLOCKWISE
+
+            elif c == 113:  # q
+                print("{}cm".format(sensor_module.get_sonar_distance() * 100))
 
             # print numeric value
             stdscr.addstr(str(c) + ' ')
@@ -95,9 +114,18 @@ def main(stdscr):
             # return curser to start position
             stdscr.move(0, 0)
 
+def crash_recovery_routine(lin_speed, turn_speed, direction):
+    print("crash recovery started")
+    ldps = movement.ldps
+    rdps = movement.rdps
+    movement.move_linear(0.15, lin_speed)
+    movement.turn(25, direction * turn_speed)
+    movement.set_left_dps(ldps)
+    movement.set_right_dps(rdps)
+
 
 if __name__ == '__main__':
     try:
         curses.wrapper(main)
     except KeyboardInterrupt:
-        robot.reset()
+        roboboi.reset()
--- src/sensor.py
+++ src/sensor.py
@@ -10,6 +10,9 @@ class SensorModule:
         self.BP.set_sensor_type(ltouch, self.BP.SENSOR_TYPE.TOUCH)
         self.BP.set_sensor_type(rtouch, self.BP.SENSOR_TYPE.TOUCH)
         self.BP.set_sensor_type(sonar, self.BP.SENSOR_TYPE.NXT_ULTRASONIC)
+
+        self.smooth_size = 10
+        self.sonar_frames = []
     
     def reset(self):
         self.BP.reset_all()
@@ -28,7 +31,21 @@ class SensorModule:
 
     def get_sonar_distance(self):
         try:
-            return self.BP.get_sensor(self.sonar) / 100
+            dist = self.BP.get_sensor(self.sonar) / 100
+            self.sonar_frames.insert(0, dist)
+            if len(self.sonar_frames) > self.smooth_size:
+                self.sonar_frames.pop()
+            
+            return dist
+
         except brickpi3.SensorError:
+            return -1
+    
+    def get_sonar_smooth(self):
+        self.get_sonar_distance()
+        frames = len(self.sonar_frames)
+        if (frames > 0):
+            return sum(self.sonar_frames) / frames
+        else:
             return -1
 
