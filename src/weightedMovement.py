ffbc5fc77aa5f5e8d34c888c2c44402f6380eb8a
seb-fenton <seb.fenton@gmail.com>
2019-11-06 18:52:28 +0000

    fucking formatting

--- src/weightedMovement.py
+++ src/weightedMovement.py
@@ -8,39 +8,39 @@ import weightedParticles
 
 def weightedMovement():
 
-targetPosX = input("X co-ord?")
-targetPosY = input("Y co-ord?")
+    targetPosX = input("X co-ord?")
+    targetPosY = input("Y co-ord?")
 
-targetPosX = float(targetPosX)
-targetPosY = float(targetPosY)
+    targetPosX = float(targetPosX)
+    targetPosY = float(targetPosY)
 
-particles = [0,0,0] * 100
+    particles = [0,0,0] * 100
 
-motor_port_left = brickpi3.BrickPi3.PORT_D
-motor_port_right = brickpi3.BrickPi3.PORT_A
-touch_port_left = brickpi3.BrickPi3.PORT_4
-touch_port_right = brickpi3.BrickPi3.PORT_1
-sonar_port = brickpi3.BrickPi3.PORT_2
+    motor_port_left = brickpi3.BrickPi3.PORT_D
+    motor_port_right = brickpi3.BrickPi3.PORT_A
+    touch_port_left = brickpi3.BrickPi3.PORT_4
+    touch_port_right = brickpi3.BrickPi3.PORT_1
+    sonar_port = brickpi3.BrickPi3.PORT_2
 
-wheel_radius = 3.5 / 100 # 3.5cm
-body_radius = 6.5 / 100 # cm
+    wheel_radius = 3.5 / 100 # 3.5cm
+    body_radius = 6.5 / 100 # cm
 
-movement_module = movement.MovementModule(motor_port_left, motor_port_right, wheel_radius, body_radius)
-sensor_module = sensor.SensorModule(touch_port_left, touch_port_right, sonar_port)
-roboboi = robot.Robot(movement_module, sensor_module)
+    movement_module = movement.MovementModule(motor_port_left, motor_port_right, wheel_radius, body_radius)
+    sensor_module = sensor.SensorModule(touch_port_left, touch_port_right, sonar_port)
+    roboboi = robot.Robot(movement_module, sensor_module)
 
-ideal_dist = 0.3
+    ideal_dist = 0.3
 
-sum = (0, 0, 0)
+    sum = (0, 0, 0)
 
-for i in range(len(particles)):
-    for q in range(3):
-        sum += particles[i][q] / len(particles)
+    for i in range(len(particles)):
+        for q in range(3):
+            sum += particles[i][q] / len(particles)
 
-roboboi.update_pos(sum[0], sum[1], sum[2])
+    roboboi.update_pos(sum[0], sum[1], sum[2])
 
-vector = Vector2(targetPosX, targetPosY)
-roboboi.move_to_pos(vector, 0.2, 45)
+    vector = Vector2(targetPosX, targetPosY)
+    roboboi.move_to_pos(vector, 0.2, 45)
 
 if __name__ == "__main__":
     weightedMovement()
