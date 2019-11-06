b5f1649624c9d5a4fb63608a25e4643584f23da0
Yusuf Ismail <yusuf@qfsw.co.uk>
2019-11-06 10:37:12 +0000

    Added Vector2 class

---
+++ src/vector2.py
@@ -0,0 +1,27 @@
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other): 
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other): 
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __iadd__(self, other): 
+        self.x += other.x
+        self.y += other.y
+
+    def __isub__(self, other): 
+        self.x -= other.x
+        self.y -= other.y
+
+    def __eq__(self, other): 
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other): 
+        return not (self == other)
+
+    def __str__(self): 
+        return self.x, self.y
