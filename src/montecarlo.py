fdf36cb438b635702561c9a47dcfc849e33bba5b
ethansk <ethaneus99@gmail.com>
2019-11-07 14:23:42 +0000

    pi keeps dc'ing

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -3,12 +3,12 @@ import particleDataStructures
 
 
 def calculate_likelihood(x, y, theta, z):
-    pass
+    nearest_wall = find_nearest_wall(x, y, theta, z, )
+    m = calculate_distance()
 
 
 # return (x0, y0, x1, y1)
 def find_nearest_wall(x, y, theta, z, map):
-
     # loop through all the walls, calculate the distance m, and find which m is closest to the sonar measurement
     min_distance = float("inf")
     for wall in map.walls:
@@ -19,8 +19,9 @@ def find_nearest_wall(x, y, theta, z, map):
     return nearest_wall
 
 
+# calculates m
 def calculate_distance(x, y, theta, wall):
     Ax, Ay, Bx, By = wall[0], wall[1], wall[2], wall[3]
-    numerator = (By - Ay)*(Ax - x) - (Bx - Ax)*(Ay - y)
-    denomenator = (By - Ay)*math.cos(theta) - (Bx - Ax) * math.sin(theta)
+    numerator = (By - Ay) * (Ax - x) - (Bx - Ax) * (Ay - y)
+    denomenator = (By - Ay) * math.cos(theta) - (Bx - Ax) * math.sin(theta)
     return numerator/denomenator
